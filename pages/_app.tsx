e91f14294aca543e285e98549446ad93ccc890d0
Barthélemy de Mazenod <dev@m-work.co>
2022-11-30 22:48:08 +0100

    [first commit]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css'
+import { Concert_One, Paytone_One, Rubik_Glitch } from '@next/font/google'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const rubikGlitch = Rubik_Glitch({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-rubik-glitch',
+})
+
+const paytoneOne = Paytone_One({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-paytone-one',
+})
+
+const concertOne = Concert_One({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-concert-one',
+})
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <main className={`${rubikGlitch.variable} font-cursive font-normal`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
+
+export default App
